619c2e30f6db1d672a62205f834b71bb7ab94771
Harvinder Power <harvinder.power@gmail.com>
2017-09-11 18:20:37 +0100

    Updated version with comments

--- sketch_sep10a/sketch_sep10a.ino
+++ sketch_sep10a/sketch_sep10a.ino
@@ -1,22 +1,20 @@
 #include <Servo.h>
 Servo myServo;
 
-//Assigment of variable to allow for streamlined modification of code later on e.g. button_delay_rate. This can be lowered to reduce time needed to press a button to register a value.
 boolean lock_state = false;
+//using an array based system, by initialising two empty arrays to be populated with values from the user (becomes the array passcode). Then the user inputs a test sequence (input_passcode).
 int passcode[4];
 int input_passcode[4];
+
+//counting to 4 before then initiating other sections of the code
 int array_counter = 0;
+
+//Assigment of variables to allow for streamlined modification of code later on e.g. button_delay_rate. This can be lowered to reduce time needed to press a button to register a value.
 int button_delay_rate = 500;
 int servo_delay_rate = 1000;
-/*One way of doing this is to set an initial state of locked = false, then to have a number assigned to each button.
- * If the number is pressed, then the value of another variable is set to another value (x).
- * Using nested if statements, of multiple if statements, then this can be done to progress the lock progress.
- * 
- * 
- */
 
 
-
+//function to map inputs from a resistor ladder to numbers assigned to each button.
 boolean button_mapper(int value){
   if (value == 1023){
     input_passcode[array_counter] = 1;
@@ -55,8 +53,7 @@ boolean button_mapper(int value){
 
 
 
-
- 
+//setting up the servo and initialisning the serial monitor
 void setup() {
   Serial.begin(9600);
   myServo.attach(9);
@@ -65,18 +62,17 @@ void setup() {
 }
 
 void loop() {
+  
+  //probes are used to obtain values for lock_val and reset_val. reset_val is outputting a constant value of 1023 until pressed, when it becomes 0. lock_val is dependent on the ladder.
   int lock_val = analogRead(A0);
   int reset_val = analogRead(A1);
-  Serial.print("Resetval=");
-  Serial.print(reset_val);
-  Serial.println();
-  /*Serial.println("Lock val =");
-  Serial.print(lock_val);*/
+  
   button_mapper(lock_val);
   
   //Serial.println("Array length =");
   //Serial.print(array_counter);
-  
+
+  //printing out the passcode
   Serial.print("Input Passcode =");
   Serial.print(input_passcode[0]);
   Serial.print(input_passcode[1]);
@@ -85,12 +81,14 @@ void loop() {
   
   //once there are 4 values in the input_passcode array, the array passcode is set to be equal to the array input_passcode, iterating through values using a for loop.
   if (lock_state == false){
+    //if the system has not been locked, and receives 4 numbers, then the system is locked
     if (array_counter == 4){
       myServo.write(90);
       delay(servo_delay_rate);
     for (int n=0; n<4; n++){
+      //the 4 numbers inputted by the user were initially held in the array input_passcode, however, this moves them to the passcode array
       passcode[n] = input_passcode[n];
-      // iterate through the values held in the array input_passcode, and reset them all back to 0, ready to receive the test passcode from the user.
+      // iterate through the values held in the array input_passcode, and reset them all back to 0, ready to receive the passcode from the user to test against the array "passcode".
       input_passcode[n] = 0;
     }
     //print out the newly set passcode
@@ -100,14 +98,15 @@ void loop() {
     Serial.print(passcode[2]);
     Serial.print(passcode[3]);
     lock_state = true;
-    Serial.println("lock state set to");
-    Serial.print(lock_state);
+    //Serial.println("lock state set to");
+    //Serial.print(lock_state);
     array_counter = 0;
   }
   }
     
   //if the system is locked, and there have been 4 values inputted, then checks to see if all numbers are correct, and in the right sequence.
   if (lock_state == true && array_counter == 4){
+    //for loop used to set the array back to 0, enabling the passcode to be entered again.
     for (int n=0; n<4; n++){
       if (input_passcode[0] == passcode[0] && input_passcode[1] == passcode[1] && input_passcode[2] == passcode[2] && input_passcode[3] == passcode[3]){
         Serial.println("Passcode verified");
